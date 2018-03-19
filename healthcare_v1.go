24169d73b30a159a3897ed475a8cd5577238eaec
phani87 <phanivasudev@gmail.com>
2018-03-19 13:23:39 -0400

    Updated Go code

--- healthcare_v1.go
+++ healthcare_v1.go
@@ -150,11 +150,12 @@ func (t *PtntVstTraceChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Resp
 		return t.initVisit(stub, args)
 	} else if function == "getHistoryForRecord" { //get history of values for a record
 		return t.getHistoryForRecord(stub, args)
-	} 
-	// else if function == "transferMobile" {
-	// 	return t.transferMobile(stub, args)
-	// }
-
+	} else if function == "getVisitByRange" { //get history of values for a record
+		return t.getVisitByRange(stub, args)
+	} else if function == "queryVisitByDoctor" {
+		return t.queryVisitByDoctor(stub, args)
+	}
+	
 	fmt.Println("invoke did not find func: " + function) //error
 	return shim.Error("Received unknown function invocation")
 }
@@ -165,12 +166,10 @@ func (t *PtntVstTraceChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Resp
 func (t *PtntVstTraceChaincode) initVisit(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var err error
 
-	// data model
-	//   0       		1      		2     		3			   4		5
-	// "Mobile", "915468752368", "Apple", "IPhone X", "1523654", "Apple Manufacturing Unit, China"
+	//{"channel":"docpharmachannel","chaincode":"healthapp_v1","method":"initVisit","args":["vi123456", "dr12345","pt852369", "rx654789", "20180316"],"chaincodeVer":"v5"}
 
 
-	// @MODIFY_HERE extend to expect 8 arguements, up from 6
+	// @MODIFY_HERE extend to expect 5 arguements, up from 6
 	if len(args) < 5 {
 		return shim.Error("Incorrect number of arguments. Expecting 7")
 	}
@@ -231,17 +230,26 @@ func (t *PtntVstTraceChaincode) initVisit(stub shim.ChaincodeStubInterface, args
 	//  The key is a composite key, with the elements that you want to range query on listed first.
 	//  In our case, the composite key is based on indexName~assember~chassisNumber.
 	//  This will enable very efficient state range queries based on composite keys matching indexName~color~*
-	indexName := "doctorId~visitId"
-	ownersIndex := "patientId~visitId"
-	err = t.createIndex(stub, indexName, []string{visit.DoctorId, visit.VisitId})
+	visitIndexbyDoc := "doctorId~visitId"
+	visitIndexbyPat := "patientId~visitId"
+	docIndexbyVisit := "visitId~doctorId"
+	patIndexbyVisit := "visitId~patientId"
+	err = t.createIndex(stub, visitIndexbyDoc, []string{visit.DoctorId, visit.VisitId})
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	err = t.createIndex(stub, ownersIndex, []string{visit.PatientId, visit.VisitId})
+	err = t.createIndex(stub, visitIndexbyPat, []string{visit.PatientId, visit.VisitId})
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-
+	err = t.createIndex(stub, docIndexbyVisit, []string{visit.VisitId, visit.DoctorId})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	err = t.createIndex(stub, patIndexbyVisit, []string{visit.VisitId, visit.PatientId})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 	// ==== Vehicle part saved and indexed. Return success ====
 	fmt.Println("- end init vehicle")
 	return shim.Success(nil)
@@ -339,155 +347,122 @@ func (t *PtntVstTraceChaincode) getHistoryForRecord(stub shim.ChaincodeStubInter
 	return shim.Success(buffer.Bytes())
 }
 
+//============================================================================================
+//Get Visit by range
+//============================================================================================
+
+func (t *PtntVstTraceChaincode) getVisitByRange(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	if len(args) < 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+
+	startKey := args[0]
+	endKey := args[1]
+
+	resultsIterator, err := stub.GetStateByRange(startKey, endKey)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getVehiclePartByRange queryResult:\n%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
+}
+
 // ===========================================================================================
-// Transfer the mobile device from various members in the supply chain
+// Query Based results
 // ===========================================================================================
 
+func (t *PtntVstTraceChaincode) queryVisitByDoctor(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-// func (t *PtntVstTraceChaincode) transferMobile(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-// 	//   0       1       3
-// 	// "name", "from", "to"
-// 	if len(args) < 3 {
-// 		return shim.Error("Incorrect number of arguments. Expecting 3")
-// 	}
+	//   0
+	// "bob"
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
 
-// 	IMEINumber := args[0]
-// 	currentOnwer := strings.ToLower(args[1])
-// 	newOwner := strings.ToLower(args[2])
-// 	fmt.Println("- start transferVehicle ", IMEINumber, currentOnwer, newOwner)
+	doctorId := strings.ToLower(args[0])
 
-// 	// attempt to get the current vehicle object by serial number.
-// 	// if sucessful, returns us a byte array we can then us JSON.parse to unmarshal
-// 	message, err := t.trannsferMobileHelper(stub, IMEINumber, currentOnwer, newOwner)
-// 	if err != nil {
-// 		return shim.Error(message + err.Error())
-// 	} else if message != "" {
-// 		return shim.Error(message)
-// 	}
+	queryString := fmt.Sprintf("{\"selector\":{\"docType\":\"visit\",\"doctorId\":\"%s\"}}", doctorId)
 
-// 	fmt.Println("- end transferVehicle (success)")
-// 	return shim.Success(nil)
-// }
+	queryResults, err := getQueryResultForQueryString(stub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(queryResults)
+}
 
-// ===========================================================
-// trannsferVehicleHelper : helper method for transferVehicle
-// ===========================================================
-// func (t *PtntVstTraceChaincode) trannsferMobileHelper(stub shim.ChaincodeStubInterface, IMEINumber string, currentOwner string, newOwner string) (string, error) {
-// 	// attempt to get the current vehicle object by serial number.
-// 	// if sucessful, returns us a byte array we can then us JSON.parse to unmarshal
-// 	fmt.Println("Transfering vehicle with chassis number: " + IMEINumber + " To: " + newOwner)
-// 	vehicleAsBytes, err := stub.GetState(IMEINumber)
-// 	if err != nil {
-// 		return "Failed to get vehicle:", err
-// 	} else if vehicleAsBytes == nil {
-// 		return "Vehicle does not exist", err
-// 	}
+// ===========================================================================================
+// Query Based results - return metod with resultset
+// ===========================================================================================
 
-// 	vehicleToTransfer := mobile{}
-// 	err = json.Unmarshal(vehicleAsBytes, &vehicleToTransfer) //unmarshal it aka JSON.parse()
-// 	if err != nil {
-// 		return "", err
-// 	}
+func getQueryResultForQueryString(stub shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
 
-// 	if currentOwner != vehicleToTransfer.Owner {
-// 		return "This asset is currently owned by another entity.", err
-// 	}
+	fmt.Printf("- getQueryResultForQueryString queryString:\n%s\n", queryString)
 
-// 	vehicleToTransfer.Owner = newOwner //change the owner
+	resultsIterator, err := stub.GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
 
-// 	vehicleJSONBytes, _ := json.Marshal(vehicleToTransfer)
-// 	err = stub.PutState(IMEINumber, vehicleJSONBytes) //rewrite the vehicle
-// 	if err != nil {
-// 		return "", err
-// 	}
+	// buffer is a JSON array containing QueryRecords
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
 
-// 	// maintain indexes
-// 	ownersIndex := "owner~identifier"
-// 	// remove previous index
-// 	err = t.deleteIndex(stub, ownersIndex, []string{currentOwner, IMEINumber})
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	// create new index
-// 	err = t.createIndex(stub, ownersIndex, []string{newOwner, IMEINumber})
-// 	if err != nil {
-// 		return "", err
-// 	}
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
 
-// 	return "", nil
-// }
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
 
+	fmt.Printf("- getQueryResultForQueryString queryResult:\n%s\n", buffer.String())
 
-// // // ===============================================
-// // // createIndex - create search index for ledger
-// // // ===============================================
-// // func (t *MobileTraceChaincode) createIndex(stub shim.ChaincodeStubInterface, indexName string, attributes []string) error {
-// // 	fmt.Println("- start create index")
-// // 	var err error
-// // 	//  ==== Index the object to enable range queries, e.g. return all parts made by supplier b ====
-// // 	//  An 'index' is a normal key/value entry in state.
-// // 	//  The key is a composite key, with the elements that you want to range query on listed first.
-// // 	//  This will enable very efficient state range queries based on composite keys matching indexName~color~*
-// // 	indexKey, err := stub.CreateCompositeKey(indexName, attributes)
-// // 	if err != nil {
-// // 		return err
-// // 	}
-// // 	//  Save index entry to state. Only the key name is needed, no need to store a duplicate copy of object.
-// // 	//  Note - passing a 'nil' value will effectively delete the key from state, therefore we pass null character as value
-// // 	value := []byte{0x00}
-// // 	stub.PutState(indexKey, value)
-
-// // 	fmt.Println("- end create index")
-// // 	return nil
-// // }
-
-// // ===============================================
-// // deleteIndex - remove search index for ledger
-// // ===============================================
-// func (t *MobileTraceChaincode) deleteIndex(stub shim.ChaincodeStubInterface, indexName string, attributes []string) error {
-// 	fmt.Println("- start delete index")
-// 	var err error
-// 	//  ==== Index the object to enable range queries, e.g. return all parts made by supplier b ====
-// 	//  An 'index' is a normal key/value entry in state.
-// 	//  The key is a composite key, with the elements that you want to range query on listed first.
-// 	//  This will enable very efficient state range queries based on composite keys matching indexName~color~*
-// 	indexKey, err := stub.CreateCompositeKey(indexName, attributes)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	//  Delete index by key
-// 	stub.DelState(indexKey)
-
-// 	fmt.Println("- end delete index")
-// 	return nil
-// }
-
-// // ===========================================================================================
-// // cryptoVerify : Verifies signed message against public key
-// // Public Key of Authority:
-// // [48 78 48 16 6 7 42 134 72 206 61 2 1 6 5 43 129 4 0 33 3 58 0 4 21 162 242 84 40 78 13 26 160 33 97 191 210 22 152 134 162 66 12 77 221 129 138 60 74 243 198 34 102 209 14 48 16 2 98 96 172 47 170 216 228 169 103 121 153 100 84 111 33 13 106 42 46 227 52 91]
-// // ===========================================================================================
-// func cryptoVerify(hash []byte, publicKeyBytes []byte, r *big.Int, s *big.Int) (result bool) {
-// 	fmt.Println("- Verifying ECDSA signature")
-// 	fmt.Println("Message")
-// 	fmt.Println(hash)
-// 	fmt.Println("Public Key")
-// 	fmt.Println(publicKeyBytes)
-// 	fmt.Println("r")
-// 	fmt.Println(r)
-// 	fmt.Println("s")
-// 	fmt.Println(s)
-
-// 	publicKey, err := x509.ParsePKIXPublicKey(publicKeyBytes)
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 		return false
-// 	}
-
-// 	switch publicKey := publicKey.(type) {
-// 	case *ecdsa.PublicKey:
-// 		return ecdsa.Verify(publicKey, hash, r, s)
-// 	default:
-// 		return false
-// 	}
-// }
+	return buffer.Bytes(), nil
+}
